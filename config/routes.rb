689486bbd49e9c6ead6f683cdc8444f270a45193
ewaight12 <ewaight12@gmail.com>
2017-05-21 13:35:49 -0400

    Fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   resources :user_friends
   resources :posts
 
-  get 'friends/:UserID', to: 'user_friends#show'
+  post 'friends', to: 'user_friends#show'
   get 'postfeed/:UserID', to: 'posts#show'
   post 'userlogin', to: 'users#login'
   post 'signup', to: 'users#create'
-  post 'postfeed/:UserID', to: 'posts#show'
+  post 'postfeed', to: 'posts#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
